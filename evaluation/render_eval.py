5cf9167a7eac07680629e166740ff3805b1e082d
kjh67 <kjh67@cam.ac.uk>
2024-05-06 18:50:05 +0100

    Added option to skip rendering

--- evaluation/render_eval.py
+++ evaluation/render_eval.py
@@ -34,7 +34,7 @@ class SplatPipelineParameters():
         self.debug = False
 
 
-def eval_splats_cuda(project_dir, iterations):
+def eval_splats_cuda(project_dir, iterations, skip_rendering):
     """Renders evaluation image, and then calls the metric calculator"""
 
     modelparams = SplatModelParameters(os.path.join(project_dir, "splat", "eval", "colmap"), 
@@ -42,22 +42,20 @@ def eval_splats_cuda(project_dir, iterations):
                                        os.path.join(project_dir, "frames"))
 
     for iteration in iterations:
-        print(f"Rendering splat iteration {iteration}")
-        with torch.no_grad():
-            gaussians = GaussianModel(modelparams.sh_degree)
-            test_scene = Scene(modelparams, gaussians, load_iteration=iteration, shuffle=False)
-
-            background=torch.tensor([0,0,0], dtype=torch.float32, device="cuda")
-
-            output_path = os.path.join(project_dir, "results", "renders", f"splat{iteration}")
-            os.makedirs(output_path, exist_ok=True)
-
-            for camera in test_scene.getTrainCameras():
-                rendering = np.moveaxis(np.array(render_gaussians(camera, gaussians, SplatPipelineParameters(), background)["render"].cpu().detach()), 0, -1) * 255
-                filename = camera.image_name + ".jpg"
-                image_path = os.path.join(output_path, filename)
-                written = cv2.imwrite(image_path, cv2.cvtColor(rendering, cv2.COLOR_RGB2BGR))
-        print("Rendering complete")
+        output_path = os.path.join(project_dir, "results", "renders", f"splat{iteration}")
+        if not skip_rendering:
+            print(f"Rendering splat iteration {iteration}")
+            with torch.no_grad():
+                gaussians = GaussianModel(modelparams.sh_degree)
+                test_scene = Scene(modelparams, gaussians, load_iteration=iteration, shuffle=False)
+                background=torch.tensor([0,0,0], dtype=torch.float32, device="cuda")
+                os.makedirs(output_path, exist_ok=True)
+                for camera in test_scene.getTrainCameras():
+                    rendering = np.moveaxis(np.array(render_gaussians(camera, gaussians, SplatPipelineParameters(), background)["render"].cpu().detach()), 0, -1) * 255
+                    filename = camera.image_name + ".jpg"
+                    image_path = os.path.join(output_path, filename)
+                    written = cv2.imwrite(image_path, cv2.cvtColor(rendering, cv2.COLOR_RGB2BGR))
+            print("Rendering complete")
         calculate_metrics("splat", iteration, os.path.join(project_dir, "frames"), output_path, os.path.join(project_dir, "results", "render_metrics"))
 
 
@@ -79,20 +77,20 @@ def render_colmap_dir_splat_openGL(ply_path, colmap_source_path, output_path):
         renderer.save_frame()
 
 
-def eval_nerfs(project_dir, iterations):
+def eval_nerfs(project_dir, iterations, skip_rendering):
     for iteration in iterations:
-        # locate folder where the config.yml file will be located
-        path_top = os.path.join(project_dir, "nerf", "unnamed", "nerfacto")
-        with os.scandir(path_top) as f:
-            timestamp_folder = next(f)
-        config_path = os.path.join(path_top, timestamp_folder)
-    
-        print(f"Rendering nerf iteration {iteration}")
         output_path = os.path.join(project_dir, "results", "renders", f"nerf{iteration}")
-        os.makedirs(output_path, exist_ok=True)
-        render_command = ["ns-render", "dataset", "--load_config", os.path.join(config_path, "config.yml"), "--output_path", output_path]
-        subprocess.run(render_command)
-        print("Rendering complete")
+        if not skip_rendering:
+            # locate folder where the config.yml file will be located
+            path_top = os.path.join(project_dir, "nerf", "unnamed", "nerfacto")
+            with os.scandir(path_top) as f:
+                timestamp_folder = next(f)
+            config_path = os.path.join(path_top, timestamp_folder)
+            print(f"Rendering nerf iteration {iteration}")
+            os.makedirs(output_path, exist_ok=True)
+            render_command = ["ns-render", "dataset", "--load_config", os.path.join(config_path, "config.yml"), "--output_path", output_path, "--rendered-output-names", "rgb"]
+            subprocess.run(render_command)
+            print("Rendering complete")
         calculate_metrics("nerf", iteration, os.path.join(project_dir, "nerf", "images"), os.path.join(output_path, "test", "rgb"), os.path.join(project_dir, "results", "render_metrics"))
 
 
@@ -133,13 +131,14 @@ if __name__ == "__main__":
                         help="Specify which iteration to generate renders (and metrics) for")
     parser.add_argument("--skip_nerfs", action='store_true')
     parser.add_argument("--skip_splats", action='store_true')
+    parser.add_argument("--skip_rendering", action='store_true')
 
     args = parser.parse_args()
 
     for target in args.target_folders:
         if not args.skip_nerfs:
             if os.path.exists(os.path.join(target, "nerf")):
-                eval_nerfs(target, args.iterations_to_evaluate)
+                eval_nerfs(target, args.iterations_to_evaluate, args.skip_rendering)
         if not args.skip_splats:
             if os.path.exists(os.path.join(target, "splat")):
-                eval_splats_cuda(target, args.iterations_to_evaluate)
+                eval_splats_cuda(target, args.iterations_to_evaluate, args.skip_rendering)
