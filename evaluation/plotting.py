49d3f893da48feafa0e0c52e93b71f1e21117634
kjh67 <kjh67@cam.ac.uk>
2024-05-07 02:47:40 +0100

    Fixed bar widths

--- evaluation/plotting.py
+++ evaluation/plotting.py
@@ -42,6 +42,7 @@ def plot_metrics(plot_data, data_labels, group_labels, y_axis_label, output_path
     # Add chart labels, legend etc
     ax.set_ylabel(y_axis_label)
     ax.set_xticks(group_positions, group_labels)
+    ax.set_xbound(0, len(data_labels))
     ax.legend(loc='upper right')
 
     plt.show()
